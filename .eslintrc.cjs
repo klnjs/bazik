ed65523a6fdaf7b76041600d4c753eb235d52cec
Rune Klein <runeklein@outlook.com>
2023-05-27 01:55:27 +0200

    ⏪ Revert

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,14 +3,5 @@ module.exports = {
 		'@klnjs/eslint-config',
 		'@klnjs/eslint-config-react',
 		'@klnjs/eslint-config-typescript'
-	],
-	root: true,
-	rules: {
-		'import/order': [
-			'error',
-			{
-				'new-lines-between': 'never'
-			}
-		]
-	}
+	]
 }
