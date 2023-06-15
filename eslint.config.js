9aafbf2bd23140a1e200df1413a7c4e9488383c2
Rune Klein <runeklein@outlook.com>
2023-06-15 19:47:44 +0200

    🚨 Allow console log in stories

--- eslint.config.js
+++ eslint.config.js
@@ -2,4 +2,14 @@ import eslintConfig from '@klnjs/eslint-config'
 import eslintConfigReact from '@klnjs/eslint-config-react'
 import eslintConfigTypescript from '@klnjs/eslint-config-typescript'
 
-export default [eslintConfig, eslintConfigReact, eslintConfigTypescript]
+export default [
+	eslintConfig,
+	eslintConfigReact,
+	eslintConfigTypescript,
+	{
+		files: '**/*.stories.tsx',
+		rules: {
+			'no-console': 'off'
+		}
+	}
+]
