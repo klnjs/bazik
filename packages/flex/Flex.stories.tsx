7352306bf122c8ebae01bc77962cd82490662573
Rune Klein <runeklein@outlook.com>
2023-06-16 12:45:48 +0200

    📝 Update Flex stories

--- packages/flex/Flex.stories.tsx
+++ packages/flex/Flex.stories.tsx
@@ -2,18 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Flex as Component } from './Flex'
 
 export default {
+	title: 'Flex',
 	component: Component
 } satisfies Meta<typeof Component>
 
 export const Flex: StoryObj<typeof Component> = {
 	render: (args) => (
-		<Component
-			sx={{ padding: 2, radius: 2 }}
-			style={{
-				border: '1px dotted black'
-			}}
-			{...args}
-		>
+		<Component {...args}>
 			<FlexChild size={40} color='red' />
 			<FlexChild size={80} color='green' />
 			<FlexChild size={60} color='blue' />
