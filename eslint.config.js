a9341b3a57e06642345ff0386faa3cc521819289
Rune Klein <runeklein@outlook.com>
2023-06-21 14:15:41 +0200

    🚨 Add node globals to script linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import eslintConfig from '@klnjs/eslint-config'
 import eslintConfigReact from '@klnjs/eslint-config-react'
 import eslintConfigTypescript from '@klnjs/eslint-config-typescript'
+import globals from 'globals'
 
 export default [
 	eslintConfig,
