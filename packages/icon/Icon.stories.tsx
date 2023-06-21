121964c72c170a2e64863a02a01fdbda39ff5f4c
Rune Klein <runeklein@outlook.com>
2023-06-21 14:18:04 +0200

    📝 Update icon stories

--- packages/icon/Icon.stories.tsx
+++ packages/icon/Icon.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { paletteProperties } from '../theme/sprinkles.css'
 import { Icon } from './Icon'
 import { IconTitle } from './IconTitle'
 import { IconDescription } from './IconDescription'
@@ -12,7 +11,7 @@ export default {
 
 export const Default: StoryObj<typeof Icon> = {
 	render: (args) => (
-		<Icon viewBox='0 0 24 24' sx={{ palette: 'error' }} {...args}>
+		<Icon viewBox='0 0 24 24' {...args}>
 			<IconTitle>Abacus</IconTitle>
 			<IconDescription>Abacus icon</IconDescription>
 			<IconPath d='M5 5H7V11H5V5M10 5H8V11H10V5M5 19H7V13H5V19M10 13H8V19H10V17H15V15H10V13M2 21H4V3H2V21M20 3V7H13V5H11V11H13V9H20V15H18V13H16V19H18V17H20V21H22V3H20Z' />
