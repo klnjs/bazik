ba7b362312470a02d69750e2a727793839f8ba95
Rune Klein <runeklein@outlook.com>
2024-02-24 22:04:03 +0100

    🔧 Update typescript

--- packages/calendar-info/system.ts
+++ packages/calendar-info/system.ts
@@ -6,7 +6,7 @@ export const getSystem = (tag: string) => {
 	if (isProperty(locale, 'getCalendars')) {
 		// @ts-expect-error getCalendars not in typescript yet
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
-		return (locale.getCalendars() as string[])[0]
+		return (locale.getCalendars() as string[])[0] ?? 'iso8601'
 	}
 
 	return new Intl.DateTimeFormat(tag).resolvedOptions().calendar
