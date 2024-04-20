4e6df82c0c384acbfd0aee49eaf60668c826153a
Rune Klein <runeklein@outlook.com>
2024-04-20 19:21:03 +0200

    🐛 Fix typescript import assets

--- apps/docs/src/docs/02-components/avatar/avatar.tsx
+++ apps/docs/src/docs/02-components/avatar/avatar.tsx
@@ -1,10 +1,11 @@
 import { Avatar, AvatarImage, AvatarFallback } from '@klnjs/avatar'
 import classes from './avatar.module.css'
+import src from './avatar.png'
 
 export default () => {
 	return (
 		<Avatar className={classes.avatar}>
-			<AvatarImage className={classes.image} />
+			<AvatarImage src={src} className={classes.image} />
 			<AvatarFallback delay={5000} className={classes.fallback}>
 				RK
 			</AvatarFallback>
