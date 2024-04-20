79bd270bd7cd3501281ab8d1e72e0c1d0283134d
Rune Klein <runeklein@outlook.com>
2024-04-20 19:23:03 +0200

    🎨 No reason

--- apps/docs/src/docs/02-components/avatar/avatar.tsx
+++ apps/docs/src/docs/02-components/avatar/avatar.tsx
@@ -1,6 +1,6 @@
 import { Avatar, AvatarImage, AvatarFallback } from '@klnjs/avatar'
-import classes from './avatar.module.css'
 import src from './avatar.png'
+import classes from './avatar.module.css'
 
 export default () => {
 	return (
