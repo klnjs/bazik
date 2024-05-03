2e879c4036041b9dd5b15cae96b8a3479bc6ddd5
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-05-04 00:06:25 +0200

    ♻️ Rework types for locale (#40)

--- packages/utils/locale/intl.d.ts
+++ packages/utils/locale/intl.d.ts
@@ -1,36 +1,11 @@
 declare namespace Intl {
-	type LocaleWeekInfo = {
-		firstDay: number
-		minimalDays: number
-		weekend: number[]
-	}
-
-	type LocaleCalendar =
-		| 'buddhist'
-		| 'chinese'
-		| 'coptic'
-		| 'dangi'
-		| 'ethioaa'
-		| 'ethiopic'
-		| 'gregory'
-		| 'hebrew'
-		| 'indian'
-		| 'islamic'
-		| 'islamic-umalqura'
-		| 'islamic-tbla'
-		| 'islamic-civil'
-		| 'islamic-rgsa'
-		| 'iso8601'
-		| 'japanese'
-		| 'persian'
-		| 'roc'
-		| 'islamicc'
-
 	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
 	interface Locale extends LocaleOptions {
 		/** Returns a weekInfo object with the properties firstDay, weekend and minimalDays for this locale. */
-		getWeekInfo?: () => LocaleWeekInfo
+		/* eslint-disable-next-line @typescript-eslint/consistent-type-imports */
+		getWeekInfo?: () => import('./src/getWeekInfo').LocaleWeekInfo
 		/** Returns a list of one or more unique calendar identifiers for this locale. */
-		getCalendars?: () => LocaleCalendar[]
+		/* eslint-disable-next-line @typescript-eslint/consistent-type-imports */
+		getCalendars?: () => import('./src/getCalendars').LocaleCalendar[]
 	}
 }
