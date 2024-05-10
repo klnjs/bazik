7687b5901f906f5b70c4b5fdb597cc1c20da9c02
KleinMann <47398070+rnkln@users.noreply.github.com>
2024-05-11 00:09:58 +0200

    ⚰️ Remove disabled and unused id from label (#61)

--- packages/react/pin/src/PinLabel.tsx
+++ packages/react/pin/src/PinLabel.tsx
@@ -1,27 +1,12 @@
-import {
-	poly,
-	forwardRef,
-	useId,
-	asDataProp,
-	type CoreProps
-} from '@klnjs/core'
+import { poly, forwardRef, type CoreProps } from '@klnjs/core'
 import { usePinContext } from './PinContext'
 
 export type PinLabelProps = CoreProps<'label'>
 
 export const PinLabel = forwardRef<'label', PinLabelProps>(
-	({ id: idProp, ...otherProps }, forwardedRef) => {
-		const id = useId(idProp)
-		const { disabled, inputId } = usePinContext()
+	(props, forwardedRef) => {
+		const { inputId } = usePinContext()
 
-		return (
-			<poly.label
-				id={id}
-				ref={forwardedRef}
-				htmlFor={inputId}
-				data-disabled={asDataProp(disabled)}
-				{...otherProps}
-			/>
-		)
+		return <poly.label ref={forwardedRef} htmlFor={inputId} {...props} />
 	}
 )
