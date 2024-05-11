5869be211b28d84eb3b94ea907f21427f5e5b0c1
Rune Klein <runeklein@outlook.com>
2024-05-11 22:59:08 +0200

    ✨ asd

--- packages/tools/basique/index.ts
+++ packages/tools/basique/index.ts
@@ -7,7 +7,7 @@ import { build } from './src/build'
 import { prepare } from './src/prepare'
 
 yargs(hideBin(process.argv))
-	.command('build', 'build a package in the repository', () => {}, build)
+	.command('build', 'build a package', () => {}, build)
 	.command('prepare', 'prepare a package for publish', () => {}, prepare)
 	.demandCommand(1)
 	.parse()
